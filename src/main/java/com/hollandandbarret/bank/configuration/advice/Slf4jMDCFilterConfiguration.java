68a13cf4e5808626373e2948b156f5e686024dbe
sshinto <97464301+sshinto@users.noreply.github.com>
2022-02-05 09:20:09 +0000

    Create Slf4jMDCFilterConfiguration.java

---
+++ src/main/java/com/hollandandbarret/bank/configuration/advice/Slf4jMDCFilterConfiguration.java
@@ -0,0 +1,31 @@
+package com.hb.bank.configuration.advice;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "slf4jfilter")
+public class Slf4jMDCFilterConfiguration {
+
+  public static final String DEFAULT_RESPONSE_TOKEN_HEADER = "X-Token";
+  public static final String DEFAULT_REQUEST_TOKEN_HEADER = "X-Token";
+  public static final String DEFAULT_MDC_ID_TOKEN_KEY = "Slf4jMDCFilter.ID";
+  public static final String DEFAULT_MDC_CLIENT_IP_KEY = "Slf4jMDCFilter.ClientIP";
+
+  private String responseHeader = DEFAULT_RESPONSE_TOKEN_HEADER;
+  private String mdcTokenKey = DEFAULT_MDC_ID_TOKEN_KEY;
+  private String mdcClientIpKey = DEFAULT_MDC_CLIENT_IP_KEY;
+  private String requestHeader = DEFAULT_REQUEST_TOKEN_HEADER;
+
+  @Bean
+  public FilterRegistrationBean servletRegistrationBean() {
+    final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+    final RequestFilter slf4jMDCFilter = new RequestFilter(responseHeader, mdcTokenKey, mdcClientIpKey, requestHeader);
+    registrationBean.setFilter(slf4jMDCFilter);
+    registrationBean.setOrder(2);
+    return registrationBean;
+  }
+}
